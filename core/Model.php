7919ba45d318c0d2f4d7f62980c864636007e198
Linh Tv <linhtvph18589@fpt.edu.vn>
2023-01-10 11:26:59 +0700

    create model and resgister model

---
+++ core/Model.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\core;
+
+class Model
+{
+    public array $errors = [];
+    public function validate($data) {
+
+    }
+}
---
+++ models/Register.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\models;
+
+use app\core\Model;
+
+class Register extends Model
+{
+
+}
